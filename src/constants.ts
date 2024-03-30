0d0b69899023a09ea063b7dd3ab2f3df83fe9e45
Rie Takahashi <megumin@megu.dev>
2024-03-30 02:56:35 +0000

    support scientific notation numbers

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,17 @@
 export const HTML_URL_REGEX = /(?:src|href)=(?:"|')(.+?)(?:"|')/g;
 export const HTML_GLOBAL_ENV_REGEX = />window\.GLOBAL_ENV\s?=\s?(\{.+?\});</gs;
 
-const JS_URL_REGEX__RSPACK = /"(?<hash>(?:\d+?\.)?\w+?\.js)"/g;
+const JS_URL_REGEX__RSPACK = /"(?<hash>(?:[\de]+?\.)?\w+?\.js)"/g;
 
 // TODO: This is ridiculous, should I give up and use AST?
 // AST is probably worse for performance, but this regex is a crime against humanity.
 const JS_URL_REGEX__27_03_2024_RSPACK_group1 =
-	/"(?<id>\d+)"===\w\?(?:""\+\w\+)?"(:?\d+)?(?<hash>\.\w+?\.js)":?/g;
+	/"(?<id>[\de]+)"===\w\?(?:""\+\w\+)?"(:?[\de]+)?(?<hash>\.\w+?\.js)":?/g;
 
 const JS_URL_REGEX__27_03_2024_RSPACK_group2 =
-	/\.js":""\+\(({(?:\d+:"\w+",?)+})\)/g;
+	/\.js":""\+\(({(?:[\de]+:"\w+",?)+})\)/g;
 
-const JS_URL_REGEX__27_03_2024_RSPACK_group2_inner = /\d+:"(?<hash>\w+)",?/g;
+const JS_URL_REGEX__27_03_2024_RSPACK_group2_inner = /[\de]+:"(?<hash>\w+)",?/g;
 
 export const JS_URL_REGEXES = {
 	rspack: JS_URL_REGEX__RSPACK,
